ecbababb103f7b378c4aa833be0845def2df9a6e
Ansh Pasricha <ansh.pasricha@outlook.com>
2025-07-14 20:56:18 +0530

    Add Data Validation

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,5 +1,7 @@
 import path from 'path';
 import ProductModel from '../models/product.model.js';
+// import { error } from 'console';
+// import { URL } from 'url';
 
 
 export default class ProductController{
@@ -13,10 +15,34 @@ export default class ProductController{
    
     }
     getAddForm(req, res) {
-       return res.render('new-product');
+       return res.render('new-product' , {errorMessage: null}); // Render the form to add a new product
     }
     
     AddNewProduct(req, res) {
+        const name = req.body.name;
+        const desc = req.body.desc;
+       
+        const imageURL = req.body.imageURL;
+        let error=[];
+        if(!name || name.ptrim()===''){
+            error.push('Name is required');
+        }
+        if(!desc || desc.trim()===''){
+            error.push('Description is required');
+        }
+        try{
+            let URL = new URL(imageURL);
+        }
+        catch(err){
+            error.push('Image URL is not valid');
+        }
+
+        if(error.length > 0) {
+            return res.render('new-product', {errorMessage :error}
+        )
+    };
+
+
 
        ProductModel.add(req.body);
        let products = ProductModel.getProducts();
