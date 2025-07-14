6d494872992319577dc4c076f30ae8026e533cb9
Ansh Pasricha <ansh.pasricha@outlook.com>
2025-07-15 01:41:13 +0530

    Validate using rxpress-validator

--- src/middlewares/validation.middleware.js
+++ src/middlewares/validation.middleware.js
@@ -1,25 +1,31 @@
-export default function validateRequest(req, res, next){
-    const name = req.body.name;
-            const desc = req.body.desc;
-           
-            const imageURL = req.body.imageURL;
-            let error=[];
-            if(!name || name.trim()===''){
-                error.push('Name is required');
-            }
-            if(!desc || desc.trim()===''){
-                error.push('Description is required');
-            }
-            try{
-                let URL = new URL(imageURL);
-            }
-            catch(err){
-                error.push('Image URL is not valid');
-            }
-    
-            if(error.length > 0) {
-                return res.render('new-product', {errorMessage :error}
+ import {body , validationResult} from 'express-validator';
+
+export default async function validateRequest(req, res, next){
+ 
+   
+
+    //1.Defining the Rules
+    const rules = [
+        body('name').notEmpty().withMessage('Name is not valid'),
+        body('price').isFloat({gt:0}).withMessage('Price is not valid'),
+        body('imageURL').isURL().withMessage('Image URL is not valid')
+    ];
+
+
+    //2.Run the rules
+    await Promise.all(rules.map(function(rule){
+        return rule.run(req);
+    }))
+
+    //3.Check for errors
+    var error = validationResult(req); //Returns an object with errors if any
+
+    console.log(error);
+
+            if(!error.isEmpty()){
+                return res.render('new-product', {errorMessage :error.array()}
             )
         };
+        next();
     
 }
